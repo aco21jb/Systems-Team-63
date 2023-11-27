7dee5fbd7d87ae1def43731da32ba46b75cb9165
Joshua Birks <JBirks1@sheffield.ac.uk>
2023-11-27 04:06:09 +0000

    Basic implementation of User class, needs editing once the login/registration views are complete

--- src/trainStore/User.java
+++ src/trainStore/User.java
@@ -1,12 +1,33 @@
 package trainStore;
 
+import java.sql.Timestamp;
+import static trainStore.util.UniqueUserIDGenerator.generateUniqueUserID;
+
 public class User {
-    public String name;
-    public String email;
-    public String phone;
-    public String address;
-    public String password;
 
+    private String userID;
+    private String email;
+    private String username;
+    private String passwordHash;
+    private Timestamp lastLogin;
+    private int failedLoginAttempts;
+    private boolean accountLocked;
+    private String name;
+    private int houseNumber;
+    private String postcode;
 
+    public User(String userID, String email, String username, String passwordHash, Timestamp lastLogin,
+                int failedLoginAttempts, boolean accountLocked, String name, int houseNumber, String postcode) {
+        this.userID = generateUniqueUserID();
+        this.email = email;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.lastLogin = lastLogin;
+        this.failedLoginAttempts = failedLoginAttempts;
+        this.accountLocked = accountLocked;
+        this.name = name;
+        this.houseNumber = houseNumber;
+        this.postcode = postcode;
+    }
 
 }
